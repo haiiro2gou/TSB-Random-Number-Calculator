25237009a2f21f39dc730921070f5f1323dcf935
blanoir3298 <mrashen.2gou@gmail.com>
2022-08-11 00:45:42 +0900

    Python

---
+++ disp_vector.cpp
@@ -0,0 +1,15 @@
+// disp_vector.cpp
+#include "disp_vector.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+double dispVector( const vector<int>& vec )
+{
+   for ( const auto& v : vec )
+      cout << v << endl;
+
+   return 0;
+}
---
+++ disp_vector.h
@@ -0,0 +1,11 @@
+// disp_vector.h
+#ifndef __DISP_VECTOR__
+#define __DISP_VECTOR__
+
+#include <vector>
+
+using std::vector;
+
+double dispVector( const vector<int>& vec );
+
+#endif
